f2376431753fc4c56be0ed41539a90b7f5a83501
Zepeng <zepengl@gmail.com>
2015-08-20 17:12:15 -0400

    trim comments

--- tau_fit.cc
+++ tau_fit.cc
@@ -215,16 +215,6 @@ void joint_fit_sys()
   //
   //systematic errors that are used in the SK Osc3++ analysis. skip t2k related errors. Each error is
   //constrained with a Gaussian function centered at 0 and sigma=1. CC_nutau_xsec is not constrained.
-  //
-  
-  // SK1 systematic errors, SK2 3&4 share part of the terms.
-  /*
-   'abs_norm_E_lt_1GeV', 'nu_nubar_ratio_E_lt_1GeV', 'nu_nubar_ratio_E_gt_10GeV', 'nuebar_nue_E_lt_1GeV', 'nuebar_nue_E_gt_10GeV', 'numubar_numu_E_lt_1GeV', 'numubar_numu_E_gt_10GeV', 'up_down_ratio', 'horizontal_vertical_ratio', 'K_pi_ratio', 'nu_path', 'abs_norm_E_gt_1GeV', 'relative_norm_FC_multi_GeV', 'axial_mass_QE_and_1pi', 'CCQE_xsec_ratio', 'single_meson_xsec', 'DIS_model_difference', 'DIS_xsec', 'coherent_pi_xsec', 'NC_CC_ratio', 'CC_nutau_xsec', 'FC_reduction_sk1', 'FC_PC_separation_sk1', 'hadron_simulation', 'non_nue_bg_elike_sk1', 'non_nu_bg_mulike_sk1', 'ring_separation_sk1', 'pid_1ring_sk1', 'pid_multi_ring_sk1', 'energy_calibration_sk1', 'up_down_energy_calibration_sk1', 'non_nue_bg_mg_1ring_elike_sk1', 'non_nue_bg_mg_multiring_elike_sk1', 'mgmre_nue_nuebar_separation_sk1', 'solar_activity_sk1', 'polfit_1_ring_pi0_sk1', 'decay_e_tagging_sk1', 'CCQE_nu_nubar_ratio', 'CCQE_numu_nue_ratio', 'pi0_qpi_ratio', 'nubar_nu_1pi_ratio', 'nu_nubar_ratio_1_E_10GeV', 'nuebar_nue_1_E_10GeV', 'numubar_numu_1_E_10GeV', 'pi_decay_tagging_error', 'fiducial_volume_sk1', 'dis_q2_high_W', 'dis_q2_low_W', 'mgmre_other_separation_sk1', 'theta13_error', 'solDm2_error', 'theta12_error', 't2k_m23_error', 't2k_s23_error', 'matter_effect_error', 'neut_axial_mass'
-   */
-  
-  // Define the variables for systematic errors, 1 means shifting the systematic error by 1 sigma
-  // The PDFs of shifting 1 sigma is built with Osc3++. 
-  // these variables should be independent of SK periods.
   
   // Read the file of histograms for systematic errors.
   TFile* fijs_sk1 = new TFile("./sys_pdf/error.sk1.root", "READ");
@@ -245,6 +235,8 @@ void joint_fit_sys()
       // read fijs for each error, and use the untrivial fijs. DO NOT constrain CC_nutau_xsec.
       {
         sk1_errors.push_back(error_term);
+        // Define the variables for systematic errors, 1 means shifting the systematic error by 1 sigma
+        // // The PDFs of shifting 1 sigma is built with Osc3++. 
         TString constraint_expr = TString::Format("RooGaussian::%s_sys(%s[-3,3], 0, 1.0)", 
                 error_term.c_str(), error_term.c_str());//Gaussian constraint.
         tau_fit->factory(constraint_expr);
