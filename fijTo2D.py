05962f0658f5a5128559595964b218fb7adeb097
Zepeng <zepengl@gmail.com>
2015-11-20 14:39:00 -0500

    remove insignificant systematic errors

--- fijTo2D.py
+++ fijTo2D.py
@@ -19,12 +19,17 @@ def get_errors(skx, sample):
     tfile.Close()
     return ErrorNames
 
+def get_max(skx, sample, error):
+    pos_max = max(convert(skx, sample, error))
+    neg_max = min(convert(skx, sample, error))
+    return max(pos_max, abs(neg_max))
+
 def write_root(skx, sample):
     errors = get_errors(skx, sample)
     print errors
     error_file = ROOT.TFile('./sys_pdf/error.sk' + str(skx) + '.' + sample + '.root','RECREATE')
     sk_type = ['I', 'II','III','IV']
-    event_file = ROOT.TFile('./objects_extra/SK-' + sk_type[skx - 1] + '.root','READ')
+    event_file = ROOT.TFile('./objects/SK-' + sk_type[skx - 1] + '.root','READ')
     dict_sample = {'fcmc':'bkg','tau':'tau'}
     events_2D = event_file.Get(dict_sample[sample] + 'HistoZenith2D')
     th1 = ROOT.TH1F('th1', 'th1', 230, 0,230)
@@ -32,6 +37,8 @@ def write_root(skx, sample):
         th1.SetBinContent(i+1,0)
     for error in errors:
         fijs = convert(skx, sample, error)
+        if get_max(skx, sample, error) < 0.01:
+            continue
         for m in range(len(fijs)):
             th1.SetBinContent(m+1, fijs[m] + th1.GetBinContent(m+1))
         th2_pos = ROOT.TH2F(error + '_pos',error + '_pos', 15, -1.0, 1.0, 15, -0.1, 1.1 )
@@ -53,6 +60,6 @@ def write_root(skx, sample):
     error_file.Close()
 
 if __name__ == '__main__':
-    for i in range(4,5):
+    for i in range(1,5):
         write_root(i, 'fcmc')
         write_root(i,'tau')
